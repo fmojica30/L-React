7171bee8aac50eb94b60c4dbe1ee598f959fb416
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2020-01-10 19:51:37 -0600

    Adding in routing to the project

---
+++ http--01-starting-setup/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import axios from '../../../Axios';
+
+import Post from '../../../components/Post/Post';
+
+import './Posts.css';
+
+
+class posts extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({
+      selectedPostId: id
+    });
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Max'
+          }
+        });
+        this.setState({
+          posts: updatedPosts
+        })
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    let posts = <p style={{textAlign:'center'}}>Something went wrong</p>
+
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            Key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+            />
+        );
+      });
+    }
+
+    return(
+      <section className="Posts">
+        {posts}
+      </section>
+    )
+  };
+}
+
+export default posts;
