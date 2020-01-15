2a5dd3c4764585aa19485399963ea60f8c6da461
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2020-01-15 14:15:55 -0600

    Routing module

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
-    }
+	state = {
+		title: null,
+		id: null
+	};
+
+	componentDidMount() {
+		const query = new URLSearchParams(this.props.location.search);
+		// console.log(this.props.location.search);
+		this.setState({title:query.get('title')});
+		console.log(this.props)
+	}
+
+	render () {
+
+		return (
+			<div>
+				<h1>{this.state.title}</h1>
+				<p>You selected the Course with ID: {this.props.match.params.courseId}</p>
+			</div>
+		);
+	}
 }
 
 export default Course;
